2149a80dc993e127188f5f847e4498e03d05cf8c
Matias-Hilaire <matuhilaire@gmail.com>
2024-10-23 19:42:57 -0300

    no entiendo q pasa pero se arreglo

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
-config({ path: '.env' });
+config({ path: '.env.local' });
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
+  schema: 'src/app/db\/schema.ts',
   out: './migrations',
   dialect: 'sqlite',
   driver: 'turso',
